e5ac43c805f3425b569c5bfebe8a6721e4b72bb9
Hebert Soares <hebertsoaresof@gmail.com>
2024-01-26 23:01:00 -0300

    Refatorando os métodos para retornar double

--- src/main/java/org/mjv/service/Calc.java
+++ src/main/java/org/mjv/service/Calc.java
@@ -3,44 +3,39 @@ import java.util.Scanner;
 
 public class Calc {
     private Scanner sc = new Scanner(System.in);
-    public void addition(){
+    public double addition(){
         System.out.println("Digite o primeiro número: ");
         double number1 = sc.nextDouble();
         System.out.println("Digita o segundo número: ");
         double number2 = sc.nextDouble();
 
-        double result = number1 + number2;
-        System.out.println("Resultado " + result);
+        return number1 + number2;
     }
 
-    public void subtraction(){
+    public double subtraction(){
         System.out.println("Digite o primeiro número: ");
         double number1 = sc.nextDouble();
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
 
-        double result = number1 - number2;
-        System.out.println("Resultado " + result);
+        return number1 - number2;
     }
 
-    public void division(){
+    public double division(){
         System.out.println("Digite o primeiro número: ");
         double number1 = sc.nextDouble();
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
 
-        double result = number1 / number2;
-        System.out.println("Resultado " + result);
+        return number1 / number2;
     }
 
-    public void multiplication(){
+    public double multiplication(){
         System.out.println("Digite o primeiro número: ");
         double number1 = sc.nextDouble();
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
 
-        double result = number1 * number2;
-        System.out.println("Resultado " + result);
-
+        return number1 * number2;
     }
 }
