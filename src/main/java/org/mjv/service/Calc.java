92bfd34f8264e58607ae9af1291f9616a5af8606
Hebert Soares <hebertsoaresof@gmail.com>
2024-01-27 20:33:27 -0300

    Criando função para exibir os históricos dos cálculos

--- src/main/java/org/mjv/service/Calc.java
+++ src/main/java/org/mjv/service/Calc.java
@@ -1,15 +1,23 @@
 package org.mjv.service;
 import java.util.Scanner;
-
+import java.util.List;
+import java.util.ArrayList;
 public class Calc {
     private Scanner sc = new Scanner(System.in);
+    private List<Double> History;
+
+    public Calc() {
+        this.History = new ArrayList<>();
+    }
     public double addition(){
         System.out.println("Digite o primeiro número: ");
         double number1 = sc.nextDouble();
         System.out.println("Digita o segundo número: ");
         double number2 = sc.nextDouble();
+        double result = number1 + number2;
+        History.add(result);
 
-        return number1 + number2;
+        return result;
     }
 
     public double subtraction(){
@@ -17,8 +25,10 @@ public class Calc {
         double number1 = sc.nextDouble();
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
+        double result = number1 - number2;
+        History.add(result);
 
-        return number1 - number2;
+        return result;
     }
 
     public double division(){
@@ -26,8 +36,10 @@ public class Calc {
         double number1 = sc.nextDouble();
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
+        double result = number1 / number2;
+        History.add(result);
 
-        return number1 / number2;
+        return result;
     }
 
     public double multiplication(){
@@ -35,7 +47,17 @@ public class Calc {
         double number1 = sc.nextDouble();
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
+        double result = number1 * number2;
+        History.add(result);
 
-        return number1 * number2;
+        return result;
     }
+
+    public void showHistory() {
+        System.out.println("Histórico de Cálculos:");
+        for (int i = 0; i < History.size(); i++) {
+            System.out.println("Cálculo " + (i + 1) + ": " + History.get(i));
+        }
+    }
+
 }
