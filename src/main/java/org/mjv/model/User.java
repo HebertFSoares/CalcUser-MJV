5f0413017c5911cbd01b4463ed9bfdec6c070a84
Hebert Soares <hebertsoaresof@gmail.com>
2024-01-26 21:52:04 -0300

    Adicionando password como campo do User

--- src/main/java/org/mjv/model/User.java
+++ src/main/java/org/mjv/model/User.java
@@ -5,10 +5,13 @@ public class User {
     private int age;
     private String work;
 
-    public User(String name, int age, String work) {
+    private String password;
+
+    public User(String name, int age, String work, String password) {
         this.name = name;
         this.age = age;
         this.work = work;
+        this.password = password;
     }
 
     public String getName() {
@@ -35,12 +38,21 @@ public class User {
         this.work = work;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", work='" + work + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
