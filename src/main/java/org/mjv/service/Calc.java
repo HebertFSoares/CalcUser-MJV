1fab3ce575ca8d9cf45b3128a11bfcec7191fe00
Hebert Soares <hebertsoaresof@gmail.com>
2024-01-27 21:00:47 -0300

    Adicionando verificação no histórico de calculo

--- src/main/java/org/mjv/service/Calc.java
+++ src/main/java/org/mjv/service/Calc.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.ArrayList;
 public class Calc {
     private Scanner sc = new Scanner(System.in);
-    private List<Double> History;
+    private List<Double> history;
 
     public Calc() {
-        this.History = new ArrayList<>();
+        this.history = new ArrayList<>();
     }
     public double addition(){
         System.out.println("Digite o primeiro número: ");
@@ -15,7 +15,7 @@ public class Calc {
         System.out.println("Digita o segundo número: ");
         double number2 = sc.nextDouble();
         double result = number1 + number2;
-        History.add(result);
+        history.add(result);
 
         return result;
     }
@@ -26,7 +26,7 @@ public class Calc {
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
         double result = number1 - number2;
-        History.add(result);
+        history.add(result);
 
         return result;
     }
@@ -37,7 +37,7 @@ public class Calc {
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
         double result = number1 / number2;
-        History.add(result);
+        history.add(result);
 
         return result;
     }
@@ -48,15 +48,19 @@ public class Calc {
         System.out.println("Digite o segundo número: ");
         double number2 = sc.nextDouble();
         double result = number1 * number2;
-        History.add(result);
+        history.add(result);
 
         return result;
     }
 
     public void showHistory() {
-        System.out.println("Histórico de Cálculos:");
-        for (int i = 0; i < History.size(); i++) {
-            System.out.println("Cálculo " + (i + 1) + ": " + History.get(i));
+        if (history.isEmpty()) {
+            System.out.println("Nenhum cálculo no histórico.");
+        } else {
+            System.out.println("Histórico de Cálculos:");
+            for (int i = 0; i < history.size(); i++) {
+                System.out.println("Cálculo " + (i + 1) + ": " + history.get(i));
+            }
         }
     }
 
